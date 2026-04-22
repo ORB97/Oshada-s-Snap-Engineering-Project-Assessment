9269946b293a13c6ba1cf51e55278c406932a382
Oshada Bandara <oshadabandara43@gmail.com>
2026-04-21 20:50:38 -0700

    Convert titles array to characters array with objects
    Refactor titles array to characters array and update related functions to handle character objects instead of strings.

--- scripts.js
+++ scripts.js
@@ -17,55 +17,94 @@
  *    JavaScript errors and logs, which is extremely helpful for debugging.
  *    (These instructions assume you're using Chrome, opening developer tools
  *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- *
+ * - 3 - Add another object to the characters array a few lines down. Reload
+ *    your browser and observe what happens. You should see a new card appear
+ *    with the character you added.
  */
 
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+const AANG_URL =
+  "https://static.wikia.nocookie.net/avatar/images/a/ae/Aang_at_Jasmine_Dragon.png";
+const KATARA_URL =
+  "https://static.wikia.nocookie.net/avatar/images/3/38/Katara.png";
+const SOKKA_URL =
+  "https://static.wikia.nocookie.net/avatar/images/a/a3/Sokka.png";
+const ZUKO_URL =
+  "https://static.wikia.nocookie.net/avatar/images/b/bd/Zuko.png";
+const TOPH_URL =
+  "https://static.wikia.nocookie.net/avatar/images/6/66/Toph_Beifong.png";
+const IROH_URL =
+  "https://static.wikia.nocookie.net/avatar/images/1/10/Iroh_smiling.png";
 
-// This is an array of strings (TV show titles)
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
+// This is an array of objects. Each object stores all the data for one character.
+// Using objects lets us group related information (name, nation, bending, etc.)
+// together instead of storing everything in separate arrays.
+let characters = [
+  {
+    name: "Aang",
+    nation: "Air Nomads",
+    bending: "All Elements (Avatar)",
+    role: "The Avatar",
+    quote: "I'm just a kid. I'm not ready to save the world.",
+    image: AANG_URL,
+  },
+  {
+    name: "Katara",
+    nation: "Water Tribe",
+    bending: "Waterbending",
+    role: "Master Waterbender & Healer",
+    quote: "I will never, ever turn my back on people who need me!",
+    image: KATARA_URL,
+  },
+  {
+    name: "Sokka",
+    nation: "Water Tribe",
+    bending: "None",
+    role: "Warrior & Strategist",
+    quote: "That's rough, buddy.",
+    image: SOKKA_URL,
+  },
+  {
+    name: "Zuko",
+    nation: "Fire Nation",
+    bending: "Firebending",
+    role: "Crown Prince / Fire Lord",
+    quote: "I've always had to struggle and fight, and that's made me strong.",
+    image: ZUKO_URL,
+  },
+  {
+    name: "Toph Beifong",
+    nation: "Earth Kingdom",
+    bending: "Earthbending & Metalbending",
+    role: "Greatest Earthbender Alive",
+    quote: "I am the greatest earthbender in the world!",
+    image: TOPH_URL,
+  },
+  {
+    name: "Iroh",
+    nation: "Fire Nation",
+    bending: "Firebending",
+    role: "General & Mentor",
+    quote: "Destiny is a funny thing. You never know how things are going to work out.",
+    image: IROH_URL,
+  },
 ];
-// Your final submission should have much more data than this, and
-// you should use more than just an array of strings to store it all.
 
-// This function adds cards the page to display the data in the array
+// This function adds cards to the page to display the data in the array
 function showCards() {
   const cardContainer = document.getElementById("card-container");
   cardContainer.innerHTML = "";
   const templateCard = document.querySelector(".card");
 
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
-
-    // This part of the code doesn't scale very well! After you add your
-    // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
+  for (let i = 0; i < characters.length; i++) {
+    let character = characters[i];
 
     const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
+    editCardContent(nextCard, character.name, character.image, character); // Edit card content
     cardContainer.appendChild(nextCard); // Add new card to the container
   }
 }
 
-function editCardContent(card, newTitle, newImageURL) {
+function editCardContent(card, newTitle, newImageURL, character) {
   card.style.display = "block";
 
   const cardHeader = card.querySelector("h2");
@@ -73,7 +112,17 @@ function editCardContent(card, newTitle, newImageURL) {
 
   const cardImage = card.querySelector("img");
   cardImage.src = newImageURL;
-  cardImage.alt = newTitle + " Poster";
+  cardImage.alt = newTitle + " Portrait";
+
+  // Fill in the bullet points with data from the character object
+  const listItems = card.querySelectorAll("li");
+  listItems[0].textContent = "Nation: " + character.nation;
+  listItems[1].textContent = "Bending: " + character.bending;
+  listItems[2].textContent = "Role: " + character.role;
+
+  // Add the character's quote below the list
+  const quoteEl = card.querySelector(".character-quote");
+  quoteEl.textContent = '"' + character.quote + '"';
 
   // You can use console.log to help you debug!
   // View the output by right clicking on your website,
@@ -81,17 +130,48 @@ function editCardContent(card, newTitle, newImageURL) {
   console.log("new card:", newTitle, "- html: ", card);
 }
 
-// This calls the addCards() function when the page is first loaded
+// This calls the showCards() function when the page is first loaded
 document.addEventListener("DOMContentLoaded", showCards);
+
+// Feature 1: Search — filters cards by character name as the user types
+function handleSearch() {
+  const searchText = document.getElementById("search-input").value.toLowerCase();
+  const allCharacters = [...characters]; // copy so we don't lose original data
+
+  characters = allCharacters.filter(function (character) {
+    return character.name.toLowerCase().includes(searchText);
+  });
+
+  showCards();
+  characters = allCharacters; // restore full array after displaying
+}
+
+// Feature 2: Filter by Nation — shows only characters from the selected nation
+function handleFilter() {
+  const selectedNation = document.getElementById("nation-filter").value;
+  const allCharacters = [...characters];
+
+  if (selectedNation === "All") {
+    showCards();
+    return;
+  }
+
+  characters = allCharacters.filter(function (character) {
+    return character.nation === selectedNation;
+  });
+
+  showCards();
+  characters = allCharacters; // restore full array after displaying
+}
 
 function quoteAlert() {
   console.log("Button Clicked!");
-  alert(
-    "I guess I can kiss heaven goodbye, because it got to be a sin to look this good!",
-  );
+  // Pick a random character quote to display
+  let randomIndex = Math.floor(Math.random() * characters.length);
+  alert(characters[randomIndex].name + ' says: "' + characters[randomIndex].quote + '"');
 }
 
 function removeLastCard() {
-  titles.pop(); // Remove last item in titles array
+  characters.pop(); // Remove last item in characters array
   showCards(); // Call showCards again to refresh
 }
